8b936ac336000828de98616021c35698ee8efb4b
omiras <oscar.miras@gmail.com>
2022-03-03 13:28:58 +0100

    fix miriam

--- ex1b.js
+++ ex1b.js
@@ -4,7 +4,7 @@ let nombre = "Pedro";
 
 // Apartado 1. Usa el operador [] en cada una de las líneas sobre la variable 'nombre' para que aparezca lo que pide el console.log
 
-console.log("Debería aparecer 'o'", nombre) // Ejemplo: console.log("Debería aparecer 'o'", nombre[5]) o nombre[nombre.length-1]
+console.log("Debería aparecer 'o'", nombre) // Ejemplo: console.log("Debería aparecer 'o'", nombre[4]) o nombre[nombre.length-1]
 
 
 console.log("Debería aparecer 'r'", nombre)
