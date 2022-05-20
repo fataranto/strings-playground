d11a9188892a011e14cb44ce0df894f0de555e0a
fataranto <fataranto@gmail.com>
2022-05-20 18:02:12 +0200

    ex2 ox

--- ex2.js
+++ ex2.js
@@ -4,7 +4,8 @@ let saludo = "Buen día!";
 
 
 for (let i = 0; i < saludo.length; i++) {
-    console.log(saludo[i]);
+    console.log(saludo[i], i);
 }
 
 // ¿Cuántas veces se ha ejecutado la línea 7 al hacer node ex2.js? ¿Por qué?
+// 9 veces, porque es la cantidad de caracteres de "Buen día!", incluido el espacio y el signo de exclamación al final
