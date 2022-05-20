ff200a1bf23475297ccc7975e4e0b4d8593fb17d
fataranto <fataranto@gmail.com>
2022-05-20 17:44:16 +0200

    ex1b ok

--- ex1b.js
+++ ex1b.js
@@ -4,9 +4,9 @@ let nombre = "Pedro";
 
 // Apartado 1. Usa el operador [] en cada una de las líneas sobre la variable 'nombre' para que aparezca lo que pide el console.log
 
-console.log("Debería aparecer 'o'", nombre) // Ejemplo: console.log("Debería aparecer 'o'", nombre[4]) o nombre[nombre.length-1]
+console.log("Debería aparecer 'o'", nombre[4]) // Ejemplo: console.log("Debería aparecer 'o'", nombre[4]) o nombre[nombre.length-1]
 
 
-console.log("Debería aparecer 'r'", nombre) // modifica solamente esta línea
+console.log("Debería aparecer 'r'", nombre[3]) // modifica solamente esta línea
 
-console.log("Debería aparecer 'P'", nombre) // modifica solamente esta línea
+console.log("Debería aparecer 'P'", nombre[0]) // modifica solamente esta línea
