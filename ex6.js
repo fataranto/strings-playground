114992a35c69008f991e8aa9a31432b284b31475
fataranto <fataranto@gmail.com>
2022-05-20 18:35:58 +0200

    finalizado

--- ex6.js
+++ ex6.js
@@ -3,10 +3,10 @@
 // Queremos repetir 3 veces el siguiente texto
 let gato = "Loki ";
 
-console.log("Loki Loki Loki :", gato); // <-- Modificar solo esta línea
+console.log("Loki Loki Loki :", gato.repeat(3)); // <-- Modificar solo esta línea
 
 // Queremos repetir numVeces el texto de la variable 'perro'. Si cambio el valor de la variable numVeces, tu código debería seguir funcionando!
 let numVeces = 6;
 let perro = "Huesitos "
 
-console.log("Huesitos Huesitos Huesitos Huesitos Huesitos Huesitos :", perro); //<-- Modificar solo esta línea
+console.log("Huesitos Huesitos Huesitos Huesitos Huesitos Huesitos :", perro.repeat(numVeces)); //<-- Modificar solo esta línea
