14a5cd228b993d87957c378cb1a4b5ef25a22e65
fataranto <fataranto@gmail.com>
2022-05-20 18:30:07 +0200

    ex4 ok

--- ex4.js
+++ ex4.js
@@ -1,12 +1,14 @@
 // [Dificil]: Hay un error en este bucle. Cambía SOLAMENTE una línea para conseguir que el bucle funcione. Debería aparecer un mensaje por consola: "arepa contiene la letra p". PISTA. Usa tantos console.log como necesites para saber qué pasa dentro del bucle. Pista: haz un console.log de palabraSecreta[i] al inicio del bucle.
 
 let encontrado = false;
-let letraBuscada = "y";
+let letraBuscada = "r";
 let palabraSecreta = "arepa";
 let i = 0;
 
 while (!encontrado && i < palabraSecreta.length) {
-    encontrado = (palabraSecreta[i] != letraBuscada)
+    //console.log(palabraSecreta[i]);
+    encontrado = (palabraSecreta[i] == letraBuscada) //no estaba comparando correctamente (faltaba un "=")
+    //console.log(encontrado);
     i++;
 }
 
